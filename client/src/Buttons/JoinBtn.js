5f4955987cd166508a872e64da8336324eb83b6e
“Samuel <sam.laroche93@gmail.com>
2021-12-09 16:18:04 -0500

    navigateToRoomPage

--- client/src/Buttons/JoinBtn.js
+++ client/src/Buttons/JoinBtn.js
@@ -1,6 +1,24 @@
 import React from "react";
 import { ModalCardBtn } from "../ComponentStylings/ButtonsStyles";
+import { useNavigate } from "react-router-dom";
+
 const JoinBtn = ({ room }) => {
-  return <ModalCardBtn>Join!</ModalCardBtn>;
+  let navigate = useNavigate();
+  const handleClick = () => {
+    if (room.roomType === "private") {
+    } else {
+      navigate(`/${room.roomType}/${room._id}`);
+    }
+  };
+
+  return (
+    <ModalCardBtn
+      onClick={() => {
+        handleClick();
+      }}
+    >
+      Join!
+    </ModalCardBtn>
+  );
 };
 export default JoinBtn;
